1401ae4737627ca47e0cde4867d91d4b9a8ade71
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2023-05-16 23:20:10 +0800

    Create Admin Middleware

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,18 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    Route::middleware(['is_admin'])->group(function () {
+        Route::get('/debug', function () {
+            return 'This page cannot be access if not admin.';
+        });
+    });
+});
